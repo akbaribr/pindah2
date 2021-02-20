b15673d071e753aaf1b60544b5760db254e40f14
akbaribr <71860780+akbaribr@users.noreply.github.com>
2021-02-20 21:58:28 +0700

    Update deteksipluss.py

--- deteksipluss.py
+++ deteksipluss.py
@@ -27,3 +27,22 @@ def gerak():
     GPIO.cleanup()
 
 img = cv2.imread('/home/ubuntu/ip/data/foto1.jpg')
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+lower_red = np.array([0, 109, 250])
+upper_red = np.array([22, 167, 255])
+
+mask = cv2.inRange(hsv, lower_red, upper_red)
+kernel = np.ones((5, 5), np.uint8)
+mask = cv2.erode(mask, kernel)
+mask = cv2.dilate(mask, kernel, iterations = 3)
+
+# Contours detection
+contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    approx = cv2.approxPolyDP(cnt, 0.05*cv2.arcLength(cnt, True), True)
+    if len(approx) == 4:
+        gerak()
